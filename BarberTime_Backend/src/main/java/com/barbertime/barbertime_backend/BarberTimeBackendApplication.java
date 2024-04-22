89a6cfaf21fd06f999d13492a6b41c8e8b913903
Marouane LASMAK <116379216+MAROUANELK02@users.noreply.github.com>
2024-04-22 12:48:12 +0100

    v1.0

---
+++ BarberTime_Backend/src/main/java/com/barbertime/barbertime_backend/BarberTimeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.barbertime.barbertime_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberTimeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarberTimeBackendApplication.class, args);
+    }
+
+}
